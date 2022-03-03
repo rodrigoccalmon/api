488ff6aecc52fb72224d8a1492d0ba2bcb8b31db
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-03-03 20:09:48 -0300

    usuarioskillpk e entidade usuarioskil

---
+++ src/main/java/com/rodrigocalmon/nekibackend/entities/UsuarioSkill.java
@@ -0,0 +1,82 @@
+package com.rodrigocalmon.nekibackend.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class UsuarioSkill implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@JsonIgnore
+	@EmbeddedId
+	private UsuarioSkillPK id = new UsuarioSkillPK();
+	private Integer nivelConhecimento;
+
+	@CreationTimestamp
+	private Date criadoEm;
+
+	@UpdateTimestamp
+	private Date atualizadoEm;
+
+	public UsuarioSkill(Usuario usuario, Skill skill, Integer nivelConhecimento) {
+		super();
+		id.setUsuario(usuario);
+		id.setSkill(skill);
+		this.nivelConhecimento = nivelConhecimento;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		id.setUsuario(usuario);
+	}
+
+	public void setSkill(Skill skill) {
+		id.setSkill(skill);
+	}
+
+	public Usuario getUsuario() {
+		return id.getUsuario();
+	}
+
+	public Skill getSkill() {
+		return id.getSkill();
+	}
+	public UsuarioSkillPK getId() {
+		return id;
+	}
+	public void setId(UsuarioSkillPK id) {
+		this.id = id;
+	}
+
+	public Integer getNivelConhecimento() {
+		return nivelConhecimento;
+	}
+
+	public void setNivelConhecimento(Integer nivelConhecimento) {
+		this.nivelConhecimento = nivelConhecimento;
+	}
+
+	public Date getCriadoEm() {
+		return criadoEm;
+	}
+
+	public void setCriadoEm(Date criadoEm) {
+		this.criadoEm = criadoEm;
+	}
+
+	public Date getAtualizadoEm() {
+		return atualizadoEm;
+	}
+
+	public void setAtualizadoEm(Date atualizadoEm) {
+		this.atualizadoEm = atualizadoEm;
+	}
+	
+}
---
+++ src/main/java/com/rodrigocalmon/nekibackend/entities/UsuarioSkillPK.java
@@ -0,0 +1,38 @@
+package com.rodrigocalmon.nekibackend.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class UsuarioSkillPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_skill")
+	private Skill skill;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Skill getSkill() {
+		return skill;
+	}
+
+	public void setSkill(Skill skill) {
+		this.skill = skill;
+	}
+	
+	
+}
