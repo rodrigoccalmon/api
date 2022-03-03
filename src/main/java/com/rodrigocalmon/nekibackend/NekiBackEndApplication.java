d38712b5449c160a4b410e58f8b92d33db4dc901
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-03-03 15:44:55 -0300

    projeto criado, pom e application properties atualizado

---
+++ src/main/java/com/rodrigocalmon/nekibackend/NekiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigocalmon.nekibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekiBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekiBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigocalmon/nekibackend/NekiBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigocalmon.nekibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekiBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
